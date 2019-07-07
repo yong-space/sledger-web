e38bce320dc3a051e9b304cb9f2e37745203fee5
Yong Sheng Tan <tanys@outlook.sg>
2019-07-07 15:56:30 +0800

    Create React App

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Sledger 2.0
         </a>
       </header>
     </div>
