cf8e4096a3e09c5ddb36ca8bb83a6096f786dce3
Yong Sheng Tan <tanys@outlook.sg>
2019-08-12 18:40:09 +0800

    Added persistent state

--- src/components/LoginContext/index.js
+++ src/components/LoginContext/index.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import createPersistedState from 'use-persisted-state';
 const LoginContext = React.createContext([{}, () => {}]);
 
 const LoginProvider = (props) => {
-    const [ state, setState ] = useState({});
+    const useLoginState = createPersistedState('login');
+    const [ state, setState ] = useLoginState({});
+
     return (
         <LoginContext.Provider value={[ state, setState ]}>
             { props.children }
