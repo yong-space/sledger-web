07405d5516644eff4a8212fd67f8f9c5e74fed7f
Yong Sheng Tan <tanys@outlook.sg>
2019-09-15 15:16:26 +0800

    Added vector logos

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,52 +1,73 @@
 import React from 'react';
 import {Button, Layout, Menu} from 'antd';
 import useLogin from '../LoginHook';
+import logoWhite from '../../assets/logo-white.svg';
+import logoBlack from '../../assets/logo-black.svg';
 
 export default () => {
-    const { Header, Content, Sider } = Layout;
     const { isLoginValid, getUsername, logout } = useLogin();
+    const menuItems = [ 'Dashboard', 'Transactions', 'Settings' ];
+    const getMenuItems = () => menuItems.map((menuItem, index) =>
+        <Menu.Item
+            key={index}
+            onClick={e => changeMenu(e)}
+        >
+            {menuItem}
+        </Menu.Item>
+    );
+    const changeMenu = e => {
+        console.log(menuItems[e.key]);
+    };
 
     const getLoginStatus = () => {
         if (isLoginValid()) {
             return (
-                <Header style={{ height: '46px', padding: 0 }}>
-                    <div style={{
-                        width: '120px',
-                        height: '31px',
-                        background: 'rgba(255, 255, 255, 0.2)',
-                        float: 'left',
-                        margin: '7px 20px'
-                    }} />
-                    <div style={{
-                        margin: '6px',
-                        lineHeight: '32px',
-                        float: 'right',
-                        color: 'white'
-                    }}>
-                        Logged in as {getUsername()}
-                        <Button icon="logout" type="danger" onClick={logout} style={{ marginLeft: '10px', fontWeight: 'bold' }}>
-                            Logout
-                        </Button>
-                    </div>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['1']}
-                    >
-                        <Menu.Item key="1">Dashboard</Menu.Item>
-                        <Menu.Item key="2">Transactions</Menu.Item>
-                        <Menu.Item key="3">Settings</Menu.Item>
-                    </Menu>
-                </Header>
+                <>
+                    Logged in as {getUsername()}
+                    <Button icon="logout" type="danger" onClick={logout} style={{ marginLeft: '10px', fontWeight: 'bold' }}>
+                        Logout
+                    </Button>
+                </>
             );
         } else {
-            return <div>Not Logged In</div>;
+            return <div style={{ margin: '0 10px' }}>Not Logged In</div>;
         }
     };
 
     return (
-        <div>
-            { getLoginStatus() }
-        </div>
+        <Layout.Header style={{ height: '46px', padding: 0 }}>
+            {/*
+            <div style={{
+                width: '120px',
+                height: '31px',
+                background: 'rgba(255, 255, 255, 0.2)',
+                float: 'left',
+                margin: '7px 20px'
+            }} />
+            */}
+            <img
+                src={logoWhite} alt='Sledger Logo'
+                style={{
+                    float: 'left',
+                    height: '24px',
+                    margin: '14px 25px'
+                }}
+            />
+            <div style={{
+                margin: '6px',
+                lineHeight: '32px',
+                float: 'right',
+                color: 'white'
+            }}>
+                { getLoginStatus() }
+            </div>
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['0']}
+            >
+                { getMenuItems() }
+            </Menu>
+        </Layout.Header>
     );
 }
