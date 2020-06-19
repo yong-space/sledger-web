d8055bfc40479b4c1c7e004efedc6fc8e4b311c3
Yong Sheng Tan <tanys@outlook.sg>
2020-06-19 23:13:36 +0800

    Added service worker auto-reload

--- src/index.js
+++ src/index.js
@@ -9,4 +9,17 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+    onSuccess: () => {},
+    onUpdate: registration => {
+        const registrationWaiting = registration.waiting;
+        if (registrationWaiting) {
+            registrationWaiting.postMessage({ type: 'SKIP_WAITING' });
+            registrationWaiting.addEventListener('statechange', e => {
+                if (e.target.state === 'activated') {
+                    window.location.reload();
+                }
+            });
+        }
+    }
+});
