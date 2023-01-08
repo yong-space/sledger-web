08d74deaa9b6892c87936ec519f6d6fc9dc5baca
Yong Sheng Tan <tanysh@pm.me>
2023-01-08 13:36:25 +0800

    Added PWA requirements

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Sledger",
+        short_name: "Sledger",
+        description: "The savings ledger app",
+        theme_color: "#375a7f",
+        icons: [
+          {
+            src: "192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+});
