fb101a896003781eeb8495ff049ea9e208dd5efe
Yong Sheng Tan <tanysh@pm.me>
2023-01-15 15:04:43 +0800

    Exclude endpoints from service worker interception

--- vite.config.js
+++ vite.config.js
@@ -3,36 +3,39 @@ import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Sledger",
-        short_name: "Sledger",
-        start_url: "/dash",
-        description: "The savings ledger app",
-        background_color: "#375a7f",
-        theme_color: "#375a7f",
-        icons: [
-          {
-            src: "192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "512.png",
-            sizes: "512x512",
-            type: "image/png"
-          },
-          {
-            src: "512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
-  ]
+    plugins: [
+        react(),
+        VitePWA({
+            workbox: {
+                navigateFallbackDenylist: [/^\/api.*/]
+            },
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Sledger",
+                short_name: "Sledger",
+                start_url: "/dash",
+                description: "The savings ledger app",
+                background_color: "#375a7f",
+                theme_color: "#375a7f",
+                icons: [
+                    {
+                        src: "192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    },
+                    {
+                        src: "512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
+        })
+    ]
 });
