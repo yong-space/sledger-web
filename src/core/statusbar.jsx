0f0301a0777066a726e42a3d3b625b782eaf430b
Yong Sheng Tan <tanysh@pm.me>
2023-01-24 22:32:06 +0800

    Simplified status bar

--- src/core/statusbar.jsx
+++ src/core/statusbar.jsx
@@ -1,35 +1,28 @@
-import { useRecoilState } from "recoil";
-import { atoms } from "./atoms";
-import { useTheme } from "@mui/material/styles";
-import Snackbar from "@mui/material/Snackbar";
-import SnackbarContent from "@mui/material/SnackbarContent";
-import styled from "styled-components";
-
-const CustomSnackbarContent = styled(SnackbarContent)`
-    background-color: ${(props) => props.palette[props.severity].dark};
-    color: white;
-    font-weight: bold;
-    cursor: pointer;
-    user-select: "none";
-`;
+import { atoms } from './atoms';
+import { useRecoilState } from 'recoil';
+import MuiAlert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 
 const StatusBar = () => {
-    const theme = useTheme();
-    const [status, setStatus] = useRecoilState(atoms.status);
-    const close = () => setStatus((prev) => ({ ...prev, open: false }));
+    const [ status, setStatus ] = useRecoilState(atoms.status);
+    const close = (event, reason) => reason !== 'clickaway'
+        && setStatus((prev) => ({ ...prev, open: false }));
 
     return (
         <Snackbar
             open={status.open}
             onClose={close}
-            onClick={close}
-            autoHideDuration={status.severity === 'error' ? 10000 : 2000}
+            autoHideDuration={status.severity === 'error' ? 10000 : 3000}
         >
-            <CustomSnackbarContent
-                palette={theme.palette}
+            <MuiAlert
+                elevation={6}
+                variant="filled"
+                onClose={close}
                 severity={status.severity}
-                message={status.msg}
-            />
+                sx={{ color: '#fff', userSelect: 'none' }}
+            >
+                {status.msg}
+            </MuiAlert>
         </Snackbar>
     );
 };
