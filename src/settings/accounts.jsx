64643d75dd59c05619f9e5d195ac27f468c42f1e
Yong Sheng Tan <tanysh@pm.me>
2023-04-29 11:58:48 +0800

    Add Edit Accounts

--- src/settings/accounts.jsx
+++ src/settings/accounts.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AccountsForm from './accounts-form';
 import AccountsGrid from './accounts-grid';
 import Button from '@mui/material/Button';
@@ -10,13 +10,16 @@ const Accounts = () => {
     const issuers = state.useState(state.issuers)[0];
     const [ accounts, setAccounts ] = state.useState(state.accounts);
     const [ showForm, setShowForm ] = useState(false);
+    const [ accountToEdit, setAccountToEdit ] = useState();
+
+    useEffect(() => setShowForm(!!accountToEdit), [ accountToEdit ]);
 
     return (
         <>
             <Title>Accounts</Title>
             <Stack spacing={4} pb={3}>
-                <AccountsGrid {...{ issuers, accounts, setAccounts }} />
-                { showForm && <AccountsForm {...{ issuers, accounts, setAccounts, setShowForm }} /> }
+                <AccountsGrid {...{ issuers, accounts, setAccounts, accountToEdit, setAccountToEdit }} />
+                { showForm && <AccountsForm {...{ issuers, accounts, setAccounts, setShowForm, accountToEdit, setAccountToEdit }} /> }
                 { !showForm && (
                     <Stack direction="row">
                         <Button
