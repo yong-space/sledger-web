508c590cb7c1a6e4948cda7981e0f0ac2b7819ac
Yong Sheng Tan <tanysh@pm.me>
2023-09-02 18:48:19 +0800

    Fix import grid checkbox selection quirks

--- src/transactions/transactions-import.jsx
+++ src/transactions/transactions-import.jsx
@@ -1,14 +1,13 @@
 import { CircularLoader } from '../core/loader';
 import { createFilterOptions } from '@mui/material/Autocomplete';
 import { DataGrid, useGridApiContext } from '@mui/x-data-grid';
-import { formatDate, formatNumber, formatDecimal } from '../util/formatters';
+import { formatDate, formatDecimal } from '../util/formatters';
 import { red, green, blue, grey } from '@mui/material/colors';
 import { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import api from '../core/api';
 import Autocomplete from '@mui/material/Autocomplete';
 import AutoFill from './auto-fill';
 import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
 import Dropzone from 'react-dropzone';
 import Stack from '@mui/material/Stack';
 import state from '../core/state';
@@ -43,14 +42,15 @@ const ImportZone = styled.div`
 const TransactionsImport = ({ setImportMode, selectedAccount }) => {
     const [ loading, setLoading ] = state.useState(state.loading);
     const [ importTransactions, setImportTransactions ] = useState();
-    const setPaginationModel = state.useState(state.paginationModel)[1];
     const [ transactions, setTransactions ] = state.useState(state.transactions);
-    const [ checkHeader, setCheckHeader ] = useState({ checked: true, indeterminate: false });
     const [ categories, setCategories ] = state.useState(state.categories);
     const [ categoryOptions, setCategoryOptions ] = useState([]);
     const [ subCategoryOptions, setSubCategoryOptions ] = useState([]);
     const [ categoryMap, setCategoryMap ] = useState();
-    const setSelectedRows = state.useState(state.selectedRows)[1];
+    const [ selectedRows, setSelectedRows ] = useState([]);
+    const [ paginationModel, setPaginationModel ] = useState();
+    const setParentSelectedRows = state.useState(state.selectedRows)[1];
+    const setParentPaginationModel = state.useState(state.paginationModel)[1];
     const { uploadImport, addTransaction, showStatus, getCategories, suggestRemarks } = api();
 
     useEffect(() => {
@@ -83,21 +83,22 @@ const TransactionsImport = ({ setImportMode, selectedAccount }) => {
         setLoading(true);
 
         uploadImport(data, (response) => {
-            setImportTransactions(response.map((e) => ({ ...e, selected: true })));
+            setImportTransactions(response);
+            setSelectedRows(response.map(({ id }) => id ));
             setLoading(false);
         });
     };
 
     const submitTransactions = () => {
         setLoading(true);
-        const selectedTransactions = importTransactions.filter(({ selected }) => selected);
+        const selectedTransactions = importTransactions.filter(({ id }) => selectedRows.indexOf(id) > -1);
 
         addTransaction(selectedTransactions, (response) => {
             const tx = [ ...transactions, ...response ].sort((a, b) => new Date(a.date) - new Date(b.date));
             setTransactions(tx);
-            setSelectedRows(response.map(({ id }) => id));
+            setParentSelectedRows(response.map(({ id }) => id));
             const index = tx.map(({ id }) => id).indexOf(response[0].id) + 1;
-            setPaginationModel((old) => ({ ...old, page: Math.floor(index / old.pageSize) }));
+            setParentPaginationModel((old) => ({ ...old, page: Math.floor(index / old.pageSize) }));
             setLoading(false);
             setImportMode(false);
             showStatus('success', 'Transactions imported');
@@ -121,30 +122,6 @@ const TransactionsImport = ({ setImportMode, selectedAccount }) => {
             )}
         </Dropzone>
     );
-
-    const isAllSelected = () => {
-        const total = importTransactions.length;
-        const selected = importTransactions.filter(({ selected }) => selected).length;
-        return total === selected;
-    };
-
-    const toggleSelection = (row) => {
-        row.selected = !row.selected;
-        const allSelected = isAllSelected();
-        setCheckHeader({
-            checked: allSelected,
-            indeterminate: !allSelected && importTransactions.filter(({ selected }) => selected).length > 0,
-        });
-    };
-
-    const toggleMassSelection = () => {
-        const allSelected = isAllSelected();
-        setImportTransactions((old) => old.map((e) => ({ ...e, selected: !allSelected })));
-        setCheckHeader({
-            checked: !allSelected,
-            indeterminate: false,
-        });
-    };
 
     const RemarksEditor = (props) => {
         const { id, value, field, hasFocus } = props;
@@ -227,15 +204,6 @@ const TransactionsImport = ({ setImportMode, selectedAccount }) => {
 
     const ImportGrid = () => {
         const columns = {
-            selector: {
-                field: 'selected',
-                headerName: 'Import?',
-                renderHeader: (x) => <Checkbox checked={checkHeader.checked} indeterminate={checkHeader.indeterminate} onClick={toggleMassSelection} />,
-                renderCell: ({ id, row }) => <Checkbox checked={row.selected} onClick={() => toggleSelection(row)} />,
-                width: 70,
-                sortable: false,
-                type: 'boolean',
-            },
             date: { editable: true, width: '100', field: 'date', headerName: 'Date', type: 'date', valueFormatter: formatDate },
             billingMonth: { editable: true, width: '100', field: 'billingMonth', headerName: 'Bill', type: 'date', valueFormatter: formatDate },
             forMonth: { editable: true, field: 'forMonth', headerName: 'Month' },
@@ -251,7 +219,7 @@ const TransactionsImport = ({ setImportMode, selectedAccount }) => {
             medisaveAmount: { editable: true, field: 'medisaveAmount', headerName: 'Medisave', type: 'number', alueFormatter: formatDecimal },
         };
 
-        const cashFields = [ columns.selector, columns.date, columns.amount, columns.remarks, columns.category, columns.subCategory ];
+        const cashFields = [ columns.date, columns.amount, columns.remarks, columns.category, columns.subCategory ];
         const creditFields = [ ...cashFields ];
         creditFields.splice(1, 0, columns.billingMonth);
         const retirementFields = [ columns.selector, columns.date, columns.forMonth, columns.code, columns.company, columns.amount, columns.ordinaryAmount, columns.specialAmount, columns.medisaveAmount ];
@@ -267,9 +235,22 @@ const TransactionsImport = ({ setImportMode, selectedAccount }) => {
             return newRow;
         };
 
+        const handleRowClick = (params, event) => {
+            if (!event.shiftKey || selectedRows.length === 0) {
+                return;
+            }
+            const selection = [ ...selectedRows, params.id ];
+            const first = Math.min.apply(Math, selection);
+            const last = Math.max.apply(Math, selection);
+            const length = last - first + 1;
+            setSelectedRows(Array.from({ length }, (e, i) => first + i));
+        };
+
         return (
             <ImportGridRoot>
                 <DataGrid
+                    checkboxSelection
+                    disableRowSelectionOnClick
                     autoPageSize
                     disableColumnMenu
                     showColumnRightBorder
@@ -278,6 +259,11 @@ const TransactionsImport = ({ setImportMode, selectedAccount }) => {
                     columns={columnMap[selectedAccount.type]}
                     editMode="row"
                     processRowUpdate={processRowUpdate}
+                    rowSelectionModel={selectedRows}
+                    onRowSelectionModelChange={(m) => setSelectedRows(m)}
+                    onRowClick={handleRowClick}
+                    paginationModel={paginationModel}
+                    onPaginationModelChange={(n) => setPaginationModel(n)}
                     sx={maxGridSize}
                 />
             </ImportGridRoot>
@@ -285,7 +271,7 @@ const TransactionsImport = ({ setImportMode, selectedAccount }) => {
     };
 
     const importDisabled = !importTransactions
-        || importTransactions.filter(({ selected }) => selected).length === 0
+        || selectedRows.length === 0
         || loading;
 
     return (
