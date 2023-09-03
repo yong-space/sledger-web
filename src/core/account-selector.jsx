ee4508f5f45ed8830e9b14f5d7d42f1dacc0eab0
Yong Sheng Tan <tanysh@pm.me>
2023-09-03 12:48:53 +0800

    Fix warning in credit card statement about invalid selected account

--- src/core/account-selector.jsx
+++ src/core/account-selector.jsx
@@ -5,6 +5,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import state from './state';
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
 
 const FxRoot = styled.sup`
     font-size: .8rem;
@@ -15,13 +16,22 @@ const FxRoot = styled.sup`
 const Fx = () => <FxRoot>FX</FxRoot>;
 
 const AccountSelector = ({ handleChange, disabled, sx, accountFilter }) => {
+    const [ loading, setLoading ] = useState(true);
     const issuers = state.useState(state.issuers)[0];
     const accounts = state.useState(state.accounts)[0];
-    const selectedAccount = state.useState(state.selectedAccount)[0];
+    const [ selectedAccount, setSelectedAccount ] = state.useState(state.selectedAccount);
     const getVisibleAccounts = () => accounts.filter((a) => a.visible);
     const getAccounts = () => !accountFilter ? getVisibleAccounts() :
         getVisibleAccounts().filter(accountFilter);
     const getIssuer = (id) => issuers.find(i => i.id === id);
+
+    useEffect(() => {
+        if (!selectedAccount || !getAccounts().find(({ id }) => id === selectedAccount.id)) {
+            setSelectedAccount(getAccounts()[0]);
+        } else {
+            setLoading(false);
+        }
+    }, [ accounts, selectedAccount ]);
 
     const colors = {
         'Cash': 'success',
@@ -38,7 +48,7 @@ const AccountSelector = ({ handleChange, disabled, sx, accountFilter }) => {
         </>
     );
 
-    return !(accounts && selectedAccount) ? <></> : (
+    return !(accounts && selectedAccount && !loading) ? <></> : (
         <FormControl size="small" sx={sx}>
             <InputLabel id="account-label">Account</InputLabel>
             <Select
